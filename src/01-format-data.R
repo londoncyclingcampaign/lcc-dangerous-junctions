8bbf6079c147dc3d1998d8261f7d702b52fbc9fe
danielhills <danieljahills@gmail.com>
2022-11-02 15:49:55 +0000

    setwd fn.

--- src/01-format-data.R
+++ src/01-format-data.R
@@ -1,6 +1,8 @@
 library(data.table)
 library(stats19)
 
+# set wd to file location
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 min_year <- 2011
 
@@ -16,22 +18,22 @@ process_dft_data <- function(input_file, type, min_year, output_file){
 }
 
 process_dft_data(
-  "data/raw-collision-data/dft-road-casualty-statistics-accident-1979-2021.csv",
+  "../data/raw-collision-data/dft-road-casualty-statistics-accident-1979-2021.csv",
   'Accident',
   min_year,
-  "data/collision-data/crashes.csv"
+  "../data/collision-data/crashes.csv"
 )
 
 process_dft_data(
-  "data/raw-collision-data/dft-road-casualty-statistics-casualty-1979-2021.csv",
+  "../data/raw-collision-data/dft-road-casualty-statistics-casualty-1979-2021.csv",
   'Casualty',
   min_year,
-  "data/collision-data/casualties.csv"
+  "../data/collision-data/casualties.csv"
 )
 
 process_dft_data(
-  "data/raw-collision-data/dft-road-casualty-statistics-vehicle-1979-2021.csv",
+  "../data/raw-collision-data/dft-road-casualty-statistics-vehicle-1979-2021.csv",
   'Vehicle',
   min_year,
-  "data/collision-data/vehicles.csv"
+  "../data/collision-data/vehicles.csv"
 )
