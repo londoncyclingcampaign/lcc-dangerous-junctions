0619f3dd92eaa2af93fa73abcc9d6e5bbc88a1ea
danielhills <danieljahills@gmail.com>
2024-01-13 16:58:30 +0000

    file reading from gcs

--- scripts/polars_vs_pandas.py
+++ scripts/polars_vs_pandas.py
@@ -6,10 +6,52 @@ import yaml
 import time
 import polars as pl
 import pandas as pd
+import streamlit as st
 
 from yaml import Loader
+# from st_files_connection import FilesConnection
+
+# storage_client = storage.Client(
+#     project='lcc-dangerous-junctions'
+# )
+from fsspec import filesystem
+
+
+# secrets = {"token": st.secrets}
 
 DATA_PARAMETERS = yaml.load(open("params.yaml", 'r'), Loader=Loader)
+
+
+time0 = time.time()
+for i in range(10):
+    fs = filesystem('gcs', token=st.secrets.connections.gcs.to_dict())
+    with fs.open("gs://lcc-app-data/junctions-tolerance=15.parquet", "rb") as f:
+        df = pl.read_parquet(
+            f,
+            columns=DATA_PARAMETERS['junction_app_columns'],
+        )
+    time1 = time.time()
+print(f'Polars: {time1 - time0}')
+print(sys.getsizeof(df))
+
+# time0 = time.time()
+# for i in range(10):
+#     conn = st.experimental_connection('gcs', type=FilesConnection)
+#     junctions = conn.read(
+#         "lcc-app-data/junctions-tolerance=15.parquet",
+#         input_format="parquet",
+#         engine='pyarrow',
+#         columns=DATA_PARAMETERS['junction_app_columns']
+#     )
+# time1 = time.time()
+# print(f'Pandas: {time1 - time0}')
+# print(sys.getsizeof(junctions))
+
+# source = "gs://lcc-app-data/collisions-tolerance=15.parquet"
+
+# df = pl.read_parquet(source)
+
+print(df.head())
 
 n = 10
 
@@ -51,3 +93,4 @@ time1 = time.time()
 
 print(f'Pandas - time: {time1 - time0}s')
 print(f'Pandas - size: {sys.getsizeof(junction_collisions) / (1024 * 1024)}MB')
+
